9f70c9ebaa02ab1de22b8e85051e5c45d47cea71
MehediHasan25 <ishanmehadi@gmail.com>
2019-03-04 12:43:25 +0600

    Updates

---
+++ precache-manifest.f4edb378291e8bd38af6ae78db493146.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "5b5901d5a9dba2e6ef57",
+    "url": "/contactmanager/static/js/main.5b5901d5.chunk.js"
+  },
+  {
+    "revision": "aa606ae7f4097d4d649d",
+    "url": "/contactmanager/static/js/2.aa606ae7.chunk.js"
+  },
+  {
+    "revision": "5b5901d5a9dba2e6ef57",
+    "url": "/contactmanager/static/css/main.fc2194c5.chunk.css"
+  },
+  {
+    "revision": "aa606ae7f4097d4d649d",
+    "url": "/contactmanager/static/css/2.82bccc4d.chunk.css"
+  },
+  {
+    "revision": "ee3d468ae3ea865e3fa6b3e6f82ae062",
+    "url": "/contactmanager/index.html"
+  }
+];
